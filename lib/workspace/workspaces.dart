333eafc93adc59c89da07b90247637211eb4dd09
Ahmad Mahmoud <abuhamdiehahmad@gmail.com>
2024-01-14 21:04:16 +0300

    fetched data from firestore

--- lib/workspace/workspaces.dart
+++ lib/workspace/workspaces.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-export 'workspaces.dart';
 import 'package:getdone/shared/widgets/workspace_item.dart';
+export 'workspaces.dart';
 
 class Workspaces extends StatefulWidget {
   const Workspaces({super.key});
@@ -9,108 +10,134 @@ class Workspaces extends StatefulWidget {
   State<Workspaces> createState() => _WorkspacesState();
 }
 
-class _WorkspacesState extends State<Workspaces> {
-  final List<Map<String, String>> workspaceData = [
-    {"workspaceTasks": "10", "workspaceTitle": "Operating System"},
-    {"workspaceTasks": "8", "workspaceTitle": "Frontend Course"},
-    {"workspaceTasks": "5", "workspaceTitle": "House Maintenance"},
-    {"workspaceTasks": "12", "workspaceTitle": "Physics"},
-  ];
+Stream<List<Map<String, dynamic>>> streamDataFromFirestore(
+    String collectionName) {
+  return FirebaseFirestore.instance.collection(collectionName).snapshots().map(
+        (QuerySnapshot querySnapshot) => querySnapshot.docs
+            .map((doc) => doc.data() as Map<String, dynamic>)
+            .toList(),
+      );
+}
 
+class _WorkspacesState extends State<Workspaces> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(
-                    context); // This will pop the current route off the navigator stack
-              },
-            ),
-            centerTitle: true,
-            backgroundColor: Colors.white,
-            elevation: 0,
-            title: Text(
-              "Workspaces",
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.black,
-              ),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            "Workspaces",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
             ),
           ),
-          body: SingleChildScrollView(
-            child: Center(
-              child: Container(
-                width: 500,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 24),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(24, 0, 0, 0),
-                      child: Text(
-                        "In the Lab of Productivity",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 24),
-                      ),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: 500,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(24, 0, 0, 0),
+                    child: Text(
+                      "In the Lab of Productivity",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 24),
                     ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/workspaces/create');
-                          },
-                          child: Container(
-                            height: 86,
-                            width: 484,
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                    15.0), // Adjust the radius as needed
-                              ),
-                              child: Center(
-                                  child: Text(
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/workspaces/create');
+                        },
+                        child: Container(
+                          height: 86,
+                          width: 484,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            child: Center(
+                              child: Text(
                                 "Create new Workspace",
                                 style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                              )),
-                              color: Color.fromARGB(200, 108, 123, 149),
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
                             ),
+                            color: Color.fromARGB(200, 108, 123, 149),
                           ),
                         ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
-                      child: Column(
-                        children: workspaceData.map((d) {
-                          return GestureDetector(
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
+                    child: StreamBuilder<List<Map<String, dynamic>>>(
+                      stream: streamDataFromFirestore('workspaces'),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text('Error: ${snapshot.error}'),
+                          );
+                        }
+
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+
+                        List<Map<String, dynamic>> data = snapshot.data ?? [];
+                        return Column(
+                          children: data.map((d) {
+                            return GestureDetector(
                               onTap: () {
                                 Navigator.pushNamed(
-                                    context, '/workspaces/workspace-details');
+                                  context,
+                                  '/workspaces/workspace-details',
+                                );
                               },
                               child: WorkspaceItem(
-                                tasksNum: d["workspaceTasks"]!,
-                                workspaceName: d["workspaceTitle"]!,
+                                workspaceName: d["name"],
+                                tasksNum: (d["tasksNum"]),
                                 setWidth: 484,
                                 nameFontSize: 20,
                                 numFontSize: 18,
-                              ));
-                        }).toList(),
-                      ),
+                              ),
+                            );
+                          }).toList(),
+                        );
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
